a9afc9a617526efc0b51c15215e49b3919331403
Aqua7710 <purdonwill@gmail.com>
2022-04-12 08:14:15 +1200

    Add project files.

---
+++ Exercise8A/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int counter = 0;
+Console.WriteLine("Hello user, this program will display the even numbers between 1 and 100:\n");
+Console.ReadLine(); // so that you can see the message 
+while(counter < 100)
+{
+    counter++;
+    if(counter%2 == 0)
+    {
+        Console.WriteLine(counter.ToString());
+    }
+}
+
